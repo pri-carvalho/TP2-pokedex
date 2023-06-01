2a40da0e48040de1c8750a6f49f23a8e5a1f45d1
VeigaJuliana <juliana.cegepgarneau@gmail.com>
2023-06-01 15:04:07 -0400

    La liste de Pokémon faite

--- src/components/pokemon/organisms/pokemon-list.tsx
+++ src/components/pokemon/organisms/pokemon-list.tsx
@@ -4,6 +4,7 @@ import { Box, CircularProgress, Container, Grid, Pagination } from "@mui/materia
 import PokemonCard from "@/components/pokemon/molecules/pokemon-card";
 import { useState } from "react";
 import { PokemonListAPI } from "@/types/pokemon";
+import { getData } from "@/api/pokemon-api";
 
 
 interface PokemonListProps{
@@ -12,10 +13,19 @@ interface PokemonListProps{
 
 export default function PokemonList(props: PokemonListProps) {
   const [pokemonList, setPokemonList] = useState<PokemonListAPI>(props.data);
- 
+  const [totalPages, setTotalPages] = useState<number>(props.data.count? Math.ceil(props.data.count/9) : 1)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [loading, setLoading] = useState<boolean>(false);
   
   return (
       <Container fixed>
+
+        {loading? (
+        <Box sx={{ mb:2, display:"flex", justifyContent:"center" }}>
+            <CircularProgress/>
+        </Box>
+        ) : (
+        <>
         <Box sx={{ mb:2 }}>
         <Grid container spacing={4}>
           {pokemonList?.results.map((pokemon) => (
@@ -25,6 +35,24 @@ export default function PokemonList(props: PokemonListProps) {
           ))}
         </Grid>
         </Box>
+        <Box sx={{ pt:2, display: 'flex', justifyContent: 'center' }}>
+         <Pagination siblingCount={1} count={totalPages} page={currentPage} onChange={(e, page:number) => {
+            setLoading(true)
+
+            const offset = (page - 1) * 9            
+            setCurrentPage(page)
+
+            getData(
+                `https://pokeapi.co/api/v2/pokemon?limit=9&offset=${offset}`
+                ).then((apiReturn) => {
+                    setPokemonList(apiReturn)
+                }).finally(() => {
+                    setLoading(false)
+                })
+         }} color="primary" />          
+        </Box>
+        </>
+        )}
       </Container>    
   );
 }
