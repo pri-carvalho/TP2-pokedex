bc4f9ff9fccc6952ce6778f03caa94b24de9bef4
asm22ae <117685069+asm22ae@users.noreply.github.com>
2023-06-02 11:22:18 -0400

    FINI

--- src/components/molecules/myFooter/myFooter.tsx
+++ src/components/molecules/myFooter/myFooter.tsx
@@ -3,22 +3,39 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 
 function MyFooter() {
   return (
     <footer>
       <AppBar position="static" style={{ backgroundColor: "lightgray", color: "black" }}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <img src="../../app/assets/img/pokeball.png" alt="logo" height={50} width={50} />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Toolbar>
+          <Typography variant="h8" component="div" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', height:'80px' }}>
+          <img src="/img/assets/pokeball.png" alt="logo" style={{ height:'50px', width:'50px', marginRight:'60px'}} />
+
             Travail TP2 - Techniques d’intégration 2
           </Typography>
         </Toolbar>
       </AppBar>
-      <AppBar position="static">
-        <Toolbar>
-          {/* Ici, vous pouvez mettre votre liste horizontale pour le deuxième niveau */}
+      <AppBar position="static" sx={{height:'60px'}}>
+      <Toolbar>
+        <Typography variant="body1" component="div" sx={{ marginLeft: '100px' }}>
+              Aline
+            </Typography>
+            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+              Asmae
+            </Typography>
+            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+              Juliana
+            </Typography>
+            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+              Priscila
+            </Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            <Typography variant="body2" component="div">
+              © 2023 Pokémon
+            </Typography>
         </Toolbar>
       </AppBar>
     </footer>
