9f4fb2bd222630f355a86cf81163eae978a85567
pri-carvalho <124946794+pri-carvalho@users.noreply.github.com>
2023-06-05 20:14:36 -0400

    footer update

--- src/components/molecules/myFooter/myFooter.tsx
+++ src/components/molecules/myFooter/myFooter.tsx
@@ -5,37 +5,35 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
-
 function MyFooter() {
   return (
     <footer>
       <AppBar position="static" style={{ backgroundColor: "lightgray", color: "black" }}>
         <Toolbar>
           <Typography variant="h8" component="div" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', height:'80px' }}>
-          <img src="/img/assets/pokeball.png" alt="logo" style={{ height:'50px', width:'50px', marginRight:'60px'}} />
-
+            <img src="/img/assets/pokeball.png" alt="logo" style={{ height:'50px', width:'50px', marginRight:'60px'}} />
             Travail TP2 - Techniques d’intégration 2
           </Typography>
         </Toolbar>
       </AppBar>
       <AppBar position="static" sx={{height:'60px'}}>
-      <Toolbar>
-        <Typography variant="body1" component="div" sx={{ marginLeft: '100px' }}>
-              Aline
-            </Typography>
-            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
-              Asmae
-            </Typography>
-            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
-              Juliana
-            </Typography>
-            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
-              Priscila
-            </Typography>
-            <Box sx={{ flexGrow: 1 }} />
-            <Typography variant="body2" component="div">
-              © 2023 Pokémon
-            </Typography>
+        <Toolbar>
+          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
+            Aline
+          </Typography>
+          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
+            Asmae
+          </Typography>
+          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
+            Juliana
+          </Typography>
+          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
+            Priscila
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography variant="body2" component="div">
+            © 2023 Pokémon
+          </Typography>
         </Toolbar>
       </AppBar>
     </footer>
@@ -43,3 +41,4 @@ function MyFooter() {
 }
 
 export default MyFooter;
+
