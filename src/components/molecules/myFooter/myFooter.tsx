d5d1278e85e799aa3cd0c8f3674ef7e5ab5082ee
VeigaJuliana <juliana.cegepgarneau@gmail.com>
2023-06-08 16:23:41 -0400

    Revert "Merge branch 'feature/filter' of https://github.com/pri-carvalho/TP2-pokedex into feature/filter"
    This reverts commit 6d9b5dad94d64ecfede451986ab9df0479a19a43, reversing
    changes made to 6a87c7c6b4ed282af3b87b8115382e006ac447c6.

--- src/components/molecules/myFooter/myFooter.tsx
+++ src/components/molecules/myFooter/myFooter.tsx
@@ -5,35 +5,37 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+
 function MyFooter() {
   return (
     <footer>
       <AppBar position="static" style={{ backgroundColor: "lightgray", color: "black" }}>
         <Toolbar>
-          <Typography variant="caption" component="div" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80px' }}>
-            <img src="/img/assets/pokeball.png" alt="logo" style={{ height: '50px', width: '50px', marginRight: '60px' }} />
+          <Typography variant="h8" component="div" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', height:'80px' }}>
+          <img src="/img/assets/pokeball.png" alt="logo" style={{ height:'50px', width:'50px', marginRight:'60px'}} />
+
             Travail TP2 - Techniques d’intégration 2
           </Typography>
         </Toolbar>
       </AppBar>
-      <AppBar position="static" sx={{ height: '60px' }}>
-        <Toolbar>
-          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
-            Aline
-          </Typography>
-          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
-            Asmae
-          </Typography>
-          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
-            Juliana
-          </Typography>
-          <Typography variant="body1" component="div" sx={{ margin: '0 20px' }}>
-            Priscila
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Typography variant="body2" component="div">
-            © 2023 Pokémon
-          </Typography>
+      <AppBar position="static" sx={{height:'60px'}}>
+      <Toolbar>
+        <Typography variant="body1" component="div" sx={{ marginLeft: '100px' }}>
+              Aline
+            </Typography>
+            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+              Asmae
+            </Typography>
+            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+              Juliana
+            </Typography>
+            <Typography variant="body1" component="div" sx={{ marginLeft: '20px', marginRight: '20px' }}>
+              Priscila
+            </Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            <Typography variant="body2" component="div">
+              © 2023 Pokémon
+            </Typography>
         </Toolbar>
       </AppBar>
     </footer>
